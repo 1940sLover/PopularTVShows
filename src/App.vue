9fb77e40ab333a730f673172a1214d42594d5aa6
Mary Epler <63474936+1940sLover@users.noreply.github.com>
2020-12-01 16:32:50 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
+/* 
+    Name: Mary Epler
+    Date: 11/30/2020
+    Class: CIS-131-W01
+    Assignment: Popular TV Shows
+    fileName: App.vue
+*/
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header theTitle="Popular TV Shows"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
+  },
+    mounted()
+  {
+    axios.get('https://api.themoviedb.org/3/tv/popular?api_key=2aff21adbfb438bd1147448bee501822&language=en-US&page=1')
+    .then((res) => {
+      this.tasks = res.data;
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0%;
+  padding: 0%;
 }
+
 </style>
